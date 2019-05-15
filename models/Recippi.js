9672ac5659df83696818d3c0c801b853a517e088
Daniel Chan <dchan2021@208-Engineering-10.local>
2019-05-15 08:17:59 -0700

    CHANGES MADE

--- models/Recippi.js
+++ models/Recippi.js
@@ -2,18 +2,12 @@ var mongoose = require('mongoose')
 
 var recippiSchema = mongoose.Schema({
   title: { type: String, required: true },
-  ingredients: [{
-    name: String,
-    quantity: Number
-  }],
+  ingredients: { type: String, required: true },
   type: { type: String, required: true },
   time: { type: Number, required: false },
   serving: { type: Number, required: true },
   calories: { type: Number, required: false },
-  content: [{
-    Steps: Number,
-    Directions: String
-  }],
+  content: { type: String, required: true },
   notes: { type: String, required: false },
 
 })
