91850f287d61e006e69a050aff086a179b044742
Daniel Chan <dchan2021@208-Engineering-10.local>
2019-05-17 08:43:04 -0700

    FOUND NEW POST

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const port = 3000
 const mongoose = require('mongoose')
 const Recippi = require('./models/Recippi.js')
 
+var PagesController = require('./controllers/pages.js')
+var PostsController = require('./controllers/posts.js')
+
 app.set('view engine', 'pug')
 
 mongoose.connect('mongodb://localhost:27017/recippi', {useNewUrlParser: true})
@@ -19,5 +22,7 @@ app.get('/', function (req, res) {
 app.get('/recipes', function (req, res) {
   res.render('recipe')
 })
+
+app.post('/new-post', PostsController.newPost)
 
 app.listen(port, () => console.log(`SERVER IS LISTENING ON PORT ${port}`))
